0977118e93f0396a65fcd0de21a747cd166afbd1
mmalide <mohamedmalide13@gmail.com>
2025-12-27 14:35:08 +0100

    creation du controleur,des vues et optimisation et genererPage

--- controllers/MainController.controller.php
+++ controllers/MainController.controller.php
@@ -1,51 +1,70 @@
 <?php
 class MainController
 {
+    private function genererPage($data)
+    {
+        extract($data);
+        ob_start();
+        require_once($view);
+        $page_content = ob_get_clean();
+        require_once($template);
+    }
+
     public function accueil()
     {
-        $page_description = "Description de la page d'accueil";
-        $page_title = "Titre de la page d'accueil";
-        ob_start();
-        require_once("./views/accueil.view.php");
-        $page_content = ob_get_clean();
-        require_once("views/common/template.php");
+        $data_page = [
+            "page_description" => "Description de la page d'accueil",
+            "page_title" => "Titre de la page d'accueil",
+            "view" => "views/accueil.view.php",
+            "template" => "views/common/template.php",
+        ];
+        $this->genererPage($data_page);
     }
 
     public function page1()
     {
-        $page_description = "Description de la page1";
-        $page_title = "Titre de la page1";
-        ob_start();
-        require_once("./views/page1.view.php");
-        $page_content = ob_get_clean();
-        require_once("views/common/template.php");
+        $data_page = [
+            "page_description" => "Description de la page1",
+            "page_title" => "Titre de la page1",
+            "view" => "views/page1.view.php",
+            "template" => "views/common/template.php",
+        ];
+        $this->genererPage($data_page);
     }
     public function page2()
     {
-        $page_description = "Description de la page2";
-        $page_title = "Titre de la page2";
-       ob_start();
-        require_once("./views/page2.view.php");
-        $page_content = ob_get_clean();
-        require_once("views/common/template.php");
+        $data_page = [
+            "page_description" => "Description de la page2",
+            "page_title" => "Titre de la page2",
+            "view" => "views/page2.view.php",
+            "template" => "views/common/template.php",
+        ];
+        $this->genererPage($data_page);
     }
     public function page3()
     {
-        $page_description = "Description de la page3";
-        $page_title = "Titre de la page3";
-        ob_start();
-        require_once("./views/page3.view.php");
-        $page_content = ob_get_clean();
-        require_once("views/common/template.php");
+        $data_page = [
+            "page_description" => "Description de la page3",
+            "page_title" => "Titre de la page3",
+            "view" => "views/page3.view.php",
+            "template" => "views/common/template.php",
+        ];
+        $this->genererPage($data_page);
     }
 
     public function pageErreur($msg)
     {
-        $page_description = "Page permettant de gerer les erreurs";
-        $page_title = "Page d'erreur";
-        ob_start();
-        require_once("./views/erreur.view.php");
-        $page_content = ob_get_clean();
-        require_once("views/common/template.php");
+        $data_page = [
+            "page_description" => "Page permettant de gerer les erreurs",
+            "page_title" => "Page d'erreur",
+            "msg" =>$msg,
+            // "nolan" => "Tout est possible à qui rêve, ose, travaille et n'abandonne jamais.",
+            // "rayan" => " Ceux qui ne font rien ne se trompent jamais.",
+            // "nisma" => "Je ne perds jamais. Soit je gagne, soit j'apprends.",
+            // "datas"=> ["Si vous ne risquez rien, vous prenez encore plus de risque.","Quand on ose, on se trompe souvent. Quand on n’ose pas, on se trompe toujours."],
+            "view" => "views/erreur.view.php",
+            "template" => "views/common/template.php",
+        ];
+        $this->genererPage($data_page);
     }
 }
--- views/erreur.view.php
+++ views/erreur.view.php
@@ -1,1 +1,6 @@
-<?= $msg; ?>
+<?= $msg; ?><br>
+<!-- <?= $nolan; ?><br>
+<?= $rayan; ?><br>
+<?= $nisma; ?><br>
+<?= $datas[0]; ?><br>
+<?= $datas[1]; ?><br> -->
